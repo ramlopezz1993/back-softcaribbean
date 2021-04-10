d498544a9d6ed15684367977aa0b1013a4ed4b5a
Ricardo Andrés Martínez López <ricardo.martinez@cadena.com.co>
2021-04-09 22:18:45 -0500

    Commit inicial
    Commit inicial cuando se crea el proyecto.

---
+++ src/main/java/co/com/softcaribbean/pruebasoftcaribbean/PruebaSoftcaribbeanApplication.java
@@ -0,0 +1,13 @@
+package co.com.softcaribbean.pruebasoftcaribbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSoftcaribbeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaSoftcaribbeanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/softcaribbean/pruebasoftcaribbean/PruebaSoftcaribbeanApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.softcaribbean.pruebasoftcaribbean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSoftcaribbeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
